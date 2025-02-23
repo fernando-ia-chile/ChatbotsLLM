6ff21915dd38b5d93ce91f80c1e1a39fb0e204f4
fernando-ia-chile <fernando.ia.chile@gmail.com>
2025-02-23 01:33:05 -0300

    Generada documentacion en código

--- main.py
+++ main.py
@@ -35,24 +35,30 @@ async def root( request: Request ):
 
 # Ruta de inicialización de la aplicación
 @app.websocket("/init")
+
+# Inicializa la conexión WebSocket
 async def init( websocket: WebSocket ):
     await websocket.accept()
     
     try:
+        # Bucle de recepción de mensajes
         while True:
             data = await websocket.receive_json()
-
+            # Inicia la conversación con el sistema
             await websocket.send_json( { "action": "init_system_response" } )
+            # Procesa los mensajes recibidos
             response = await process_messages( data, websocket )
             await websocket.send_json( { "action": "finish_system_response" } )
     except (WebSocketDisconnect, ConnectionClosed):
         print( "Conexión cerrada" )
 
+# Procesa los mensajes recibidos y envía la respuesta al cliente
 async def process_messages( messages, websocket ):
     completion_payload = {
         "messages": messages
     }
 
+    # Realiza la petición a la API de OpenAI para obtener la respuesta
     response = await client.chat.completions.create(
         top_p=0.9,
         temperature=0.6,
@@ -62,16 +68,17 @@ async def process_messages( messages, websocket ):
     )
 
     respStr = ""
+    # Bucle de recepción de mensajes
     async for chunk in response:
         if (not chunk.choices[0] or
             not chunk.choices[0].delta or
             not chunk.choices[0].delta.content):
           continue
-
-        await websocket.send_json( { "action": "append_system_response", "content": chunk.choices[0].delta.content } )
+          
+        await websocket.send_json( { "action": "append_system_response", "content": chunk.choices[0].delta.content } ) # Envía la respuesta al cliente
 
     return respStr
 
-
+# Inicia el servidor Uvicorn para ejecutar la aplicación FastAPI
 uvicorn.run(app, host="0.0.0.0", port=8000)
 
